3ee9e452af3458dbaae17e2b15d53be9c20a2948
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-07 19:38:52 -0400

    added Tip Calculatr exercise

--- functions.js
+++ functions.js
@@ -9,7 +9,7 @@ hello('Mustache');
 */
 
 ///Hello You, 2
-function hello(name) {
+/*function hello(name) {
   name_1 = "Hello " + name;
   if (name === undefined) {
   	console.log("Hello World");
@@ -20,3 +20,117 @@ function hello(name) {
 }
 
 hello();
+
+//MadLib
+function MadLib(name, sub) {
+  var m_sentence = name + "'s " + "favorite subjet is " + sub +".";
+  console.log(m_sentence);
+}*/
+
+//MadLib("Zach", "programing")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///Tip Calculator 2
+
+
+
+
+
+
+
+
+
+
+///Tip Calculator 3
+
+
+
+
+
+
+
+
+
+
+
+
+///Print Numbers
+/*function count1_10() {
+  numbers = [1,2,3,4,5,6,7,8,9,10]
+  for (number in numbers) {
+    console.log(number);
+  }
+}
+
+count1_10();
+*/
+
+///Do Print Numbers Exercise but with While Loop
+
+
+
+
+
+
+
+
+
+
+///Print a Square
+
+//Tip Calculator 1 and 2
+
+/*
+function tipAmount(amt,service) {
+	if (service === 'good') {
+    t_A = (amt*.2) + amt;
+  }
+  else if (service === 'fair') {
+    t_A = (amt*.15) + amt;
+  } 
+  else if (service === 'poor') {
+    t_A = (amt*.1) + amt;       
+  }
+	//t_A = t_A.toStrings();
+  monies = "$" + t_A;
+  console.log(monies);
+}
+
+tipAmount(100, 'good');
+
+*/
+
+//Tip Calculator 3
+/*
+function tipAmount(amt,service,people) {
+	if (service === 'good') {
+    t_A = ((amt*.2) + amt)/people;
+  }
+  else if (service === 'fair') {
+    t_A = ((amt*.15) + amt)/people;
+  } 
+  else if (service === 'poor') {
+    t_A = ((amt*.1) + amt)/people;       
+  }
+	//t_A = t_A.toStrings();
+  monies = "$" + t_A;
+  console.log(monies);
+}
+
+tipAmount(100, 'good', 5);
+*/
+
