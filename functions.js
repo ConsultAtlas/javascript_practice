2afd186081b90f06594205fbaf86c63944af27ff
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-07 18:00:12 -0400

    added hello you and hello you two exaples

---
+++ functions.js
@@ -0,0 +1,22 @@
+/// Hey You!
+/*function hello(name) {
+  name_1 = "Hello " + name;
+  console.log(name_1);
+}
+
+hello('Mustache');
+
+*/
+
+///Hello You, 2
+function hello(name) {
+  name_1 = "Hello " + name;
+  if (name === undefined) {
+  	console.log("Hello World");
+  }
+  else {
+  	console.log(name_1);
+  }
+}
+
+hello();
